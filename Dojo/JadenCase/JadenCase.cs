02be182564cba9f5f67dd6926bbdcd65061530d5
Petar Kanev <pvkanev@gmail.com>
2018-10-07 19:33:46 +0100

    add Jaden case kata

---
+++ Dojo/JadenCase/JadenCase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace JadenCase
+{
+    public static class JadenCase
+    {
+        private static HashSet<char> wordSeparators = new HashSet<char>
+        {
+            ' ', '.', '.', '!', '?', ',', '"', ':', ';'
+        };
+
+        public static string ToJadenCase(this string phrase)
+        {
+            string result = string.Empty;
+            for (int index = 0, phraseLength = phrase.Length; index < phraseLength; index ++)
+            {
+                if (ShouldBeUpperCase(phrase, index))
+                    result += phrase[index].ToString().ToUpper();
+                else
+                    result += phrase[index].ToString().ToLower();
+            }
+
+            return result;
+        }
+
+        private static bool ShouldBeUpperCase(string phrase, int index)
+            => index == 0 || wordSeparators.Contains(phrase[index - 1]);
+    }
+}
---
+++ Dojo/Tests/JadenCaseTests.cs
@@ -0,0 +1,26 @@
+﻿using JadenCase;
+using Xunit;
+
+namespace Tests
+{
+    public class JadenCaseTests
+    {
+        [Fact]
+        public void JadenCaseShouldReturnCorrectValues()
+        {
+            AssertResultEqualsExpected("".ToJadenCase(), string.Empty);
+            AssertResultEqualsExpected("A".ToJadenCase(), "A");
+            AssertResultEqualsExpected("a".ToJadenCase(), "A");
+            AssertResultEqualsExpected("aa".ToJadenCase(), "Aa");
+            AssertResultEqualsExpected("AA".ToJadenCase(), "Aa");
+            AssertResultEqualsExpected("a a".ToJadenCase(), "A A");
+            AssertResultEqualsExpected("a aa".ToJadenCase(), "A Aa");
+            AssertResultEqualsExpected("a a, a".ToJadenCase(), "A A, A");
+            AssertResultEqualsExpected("a a: a".ToJadenCase(), "A A: A");
+            AssertResultEqualsExpected("a.a, a".ToJadenCase(), "A.A, A");
+        }
+
+        private void AssertResultEqualsExpected(string actual, string expected)
+            => Assert.Equal(expected, actual);
+    }
+}
