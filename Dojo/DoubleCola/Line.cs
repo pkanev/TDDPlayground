b9a7b746c8cda31d59842ec7c1ecaa8854264f48
Petar Kanev <pvkanev@gmail.com>
2018-10-20 13:01:00 +0100

    Added the double cola kata

---
+++ Dojo/DoubleCola/Line.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DoubleCola
+{
+    public class Line
+    {
+        public static string WhoIsNext(string[] queue, long finalTurn)
+        {
+            const int MULTIPLY_FACTOR = 2;
+            long current = 0;
+            int step = 0;
+            long increment;
+            int queueLength = queue.Length;
+            int index = 0;
+
+            while (current < finalTurn)
+            {
+                increment = (long)Math.Pow(MULTIPLY_FACTOR, step);
+                for (index = 0; index < queueLength; index++)
+                {
+                    current += increment;
+                    if(current >= finalTurn)
+                        break;
+                }
+
+                step++;
+            }
+
+            return queue[index];
+        }
+    }
+}
---
+++ Dojo/Tests/DoubleColaTests.cs
@@ -0,0 +1,26 @@
+﻿using DoubleCola;
+using Xunit;
+
+namespace Tests
+{
+    public class DoubleColaTests
+    {
+        [Theory]
+        [InlineData("Sheldon", 1, "Sheldon")]
+        [InlineData("Leonard", 2, "Sheldon", "Leonard")]
+        [InlineData("Sheldon", 3, "Sheldon", "Leonard")]
+        [InlineData("Sheldon", 4, "Sheldon", "Leonard")]
+        [InlineData("Sheldon", 6, "Sheldon", "Leonard", "Penny", "Rajesh", "Howard")]
+        [InlineData("Penny", 52, "Sheldon", "Leonard", "Penny", "Rajesh", "Howard")]
+        [InlineData("Leonard", 7230702951, "Sheldon", "Leonard", "Penny", "Rajesh", "Howard")]
+        public void NextInLineShouldReturnCorrectPerson(string expected, long turn, params string[] queue)
+        {
+            AssertThatAreEqual(Line.WhoIsNext(queue, turn), expected);
+        }
+
+        private void AssertThatAreEqual(string actual, string expected)
+        {
+            Assert.Equal(expected, actual);
+        }
+    }
+}
