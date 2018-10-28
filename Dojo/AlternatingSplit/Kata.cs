31badc7d57455a10550975b8c2e9504432f73e06
Petar Kanev <pvkanev@gmail.com>
2018-10-28 12:41:36 +0000

    Added alternating split

---
+++ Dojo/AlternatingSplit/Kata.cs
@@ -0,0 +1,58 @@
+﻿namespace AlternatingSplit
+{
+    public static class Kata
+    {
+        public static string Encrypt(string text, int iterations)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            string result = text;
+            for (int iteration = 0; iteration < iterations; iteration ++)
+                result = EncryptOnce(result);
+
+            return result;
+        }
+
+        public static string Decrypt(string encryptedText, int iterations)
+        {
+            if (string.IsNullOrEmpty(encryptedText))
+                return encryptedText;
+
+            string result = encryptedText;
+            int splitIndex = result.Length / 2;
+            for (int iteration = 0; iteration < iterations; iteration ++)
+               result = DecryptOnce(result, splitIndex);
+
+            return result;
+        }        
+
+        private static string EncryptOnce(string text)
+        {
+            string odds = string.Empty;
+            string evens = string.Empty;
+            for (int index = 0; index < text.Length; index++)
+                if (index % 2 == 1)
+                    odds += text[index];
+                else
+                    evens += text[index];
+
+            return odds + evens;
+        }
+
+        private static string DecryptOnce(string encryptedText, int splitIndex)
+        {
+            string firstHalf = encryptedText.Substring(0, splitIndex);
+            string secondHalf = encryptedText.Substring(splitIndex);
+            string result = string.Empty;
+            for (int index = 0; index < secondHalf.Length; index++)
+            {
+                result += secondHalf[index];
+                if (index < firstHalf.Length)
+                    result += firstHalf[index];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Dojo/Tests/AlternatingSplitTests.cs
@@ -0,0 +1,38 @@
+﻿using AlternatingSplit;
+using Xunit;
+
+namespace Tests
+{
+    public class AlternatingSplitTests
+    {
+        [Theory]
+        [InlineData(null, 0, null)]
+        [InlineData("", 1, "")]
+        [InlineData("ab", 1, "ba")]
+        [InlineData("This is a test!", 0, "This is a test!")]
+        [InlineData("This is a test!", 1, "hsi  etTi sats!")]
+        [InlineData("This is a test!", 2, "s eT ashi tist!")]
+        [InlineData("This is a test!", 3, " Tah itse sits!")]
+        [InlineData("This is a test!", 4, "This is a test!")]
+        [InlineData("This is a test!", -1, "This is a test!")]
+        [InlineData("This kata is very interesting!", 1, "hskt svr neetn!Ti aai eyitrsig")]
+        public void EncryptShouldEncryptCorrectly(string input, int iterations, string expected)
+            => Assert.Equal(expected, Kata.Encrypt(input, iterations));
+
+        [Theory]
+        [InlineData(null, 0, null)]
+        [InlineData("", 1, "")]
+        [InlineData("a", 1, "a")]
+        [InlineData("ba", 1, "ab")]
+        [InlineData("This is a test!", 0, "This is a test!")]
+        [InlineData("hsi  etTi sats!", 1, "This is a test!")]
+        [InlineData("s eT ashi tist!", 2, "This is a test!")]
+        [InlineData(" Tah itse sits!", 3, "This is a test!")]
+        [InlineData("This is a test!", 4, "This is a test!")]
+        [InlineData("This is a test!", -1, "This is a test!")]
+        [InlineData("hskt svr neetn!Ti aai eyitrsig", 1, "This kata is very interesting!")]
+        public void DecryptShouldReturnOriginalValue(string encrypted, int iterations, string expected)
+            => Assert.Equal(expected, Kata.Decrypt(encrypted, iterations));
+
+    }
+}
