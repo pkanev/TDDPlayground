d4764a2dc79a73138d2e5b25e766fde2600eb2d0
Petar Kanev <pvkanev@gmail.com>
2018-10-28 11:11:15 +0000

    Added RGB to HEX

---
+++ Dojo/RGBToHex/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RGBToHex
+{
+    public static class Kata
+    {
+        private const int MIN = 0;
+        private const int MAX = 255;
+
+        public static string Rgb(int red, int green, int blue)
+        {
+            red = NormalizeColorValue(red);
+            green = NormalizeColorValue(green);
+            blue = NormalizeColorValue(blue);
+            return red.ToString("X2") + green.ToString("X2") + blue.ToString("X2");
+        }
+
+        private static int NormalizeColorValue(int color)
+        {
+            if (color > MAX)
+                return MAX;
+            if (color < MIN)
+                return MIN;
+            return color;
+        }
+    }
+}
---
+++ Dojo/Tests/RGBToHexTests.cs
@@ -0,0 +1,16 @@
+﻿using RGBToHex;
+using Xunit;
+
+namespace Tests
+{
+    public class RGBToHexTests
+    {
+        [Theory]
+        [InlineData(0, 0, 0, "000000")]
+        [InlineData(255, 255, 300, "FFFFFF")]
+        [InlineData(255, 255, -20, "FFFF00")]
+        [InlineData(212, 53, 12, "D4350C")]        
+        public void RGBShouldReturnCorrectHexadecimalValue(int red, int green, int blue, string expected)
+            => Assert.Equal(expected, Kata.Rgb(red, green, blue));
+    }
+}
