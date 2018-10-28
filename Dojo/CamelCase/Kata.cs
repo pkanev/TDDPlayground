fc5664d05556454d0a3f147e6cc83fc192d00bd8
Petar Kanev <pvkanev@gmail.com>
2018-10-28 10:37:16 +0000

    Added CamelCase Kata

---
+++ Dojo/CamelCase/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CamelCase
+{
+    public static class Kata
+    {
+        public static string CamelCase(this string input)
+        {
+            string result = string.Empty;
+            if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(input))
+            {
+                return result;
+            }
+
+            string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int index = 0; index < words.Length; index++)
+                result += CapitalizeFirstLetter(words[index]);
+            
+            return result;
+        }
+
+        private static string CapitalizeFirstLetter(string word)
+            => word[0].ToString().ToUpperInvariant() + word.Substring(1).ToLowerInvariant();
+    }
+}
---
+++ Dojo/Tests/CamelCaseTests.cs
@@ -0,0 +1,20 @@
+﻿using CamelCase;
+using Xunit;
+
+namespace Tests
+{
+    public class CamelCaseTests
+    {
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("    ", "")]
+        [InlineData("a", "A")]
+        [InlineData("aa", "Aa")]
+        [InlineData("aA", "Aa")]
+        [InlineData("a a", "AA")]
+        [InlineData("a          a", "AA")]
+        [InlineData("test if this function WorS", "TestIfThisFunctionWors")]
+        public void CamelCaseShouldReturnValidString(string input, string expected)
+            => Assert.Equal(expected, input.CamelCase());
+    }
+}
