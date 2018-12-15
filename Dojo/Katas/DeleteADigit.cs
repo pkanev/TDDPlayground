09a79a7492cfa6c1b5d509658948a8a40e8b5054
Petar Kanev <pvkanev@gmail.com>
2018-12-15 09:40:13 +0000

    Added DeleteADigit kata

---
+++ Dojo/Katas/DeleteADigit.cs
@@ -0,0 +1,40 @@
+﻿namespace Katas
+{
+    using System;
+
+    public class DeleteADigit
+    {
+        private const int MIN = 10;
+        private const int MAX = 1000000;
+
+        public int DeleteDigit(int inputNumber)
+        {
+            HandleOutOfRangeException(inputNumber);
+
+            int currentMax = int.MinValue;
+            string numberAsString = inputNumber.ToString();
+            for (int index = 0; index < numberAsString.Length; index++)
+                currentMax = UpdateMaxValue(currentMax, numberAsString, index);
+
+            return currentMax;
+        }
+
+        private void HandleOutOfRangeException(int inputNumber)
+        {
+            if (inputNumber < MIN || inputNumber > MAX)
+                throw new ArgumentOutOfRangeException($"{nameof(inputNumber)} should be between {MIN} and {MAX}");
+        }
+
+        private int UpdateMaxValue(int currentMax, string number, int index)
+        {
+            int currentNumber = GetModifiedNumber(number, index);
+            if (currentNumber > currentMax)
+                currentMax = currentNumber;
+
+            return currentMax;
+        }
+
+        private static int GetModifiedNumber(string number, int index)
+            => int.Parse(number.Substring(0, index) + number.Substring(index + 1));
+    }
+}
---
+++ Dojo/Tests/DeleteADigitTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Tests
+{
+    using System;
+    using Katas;
+    using Xunit;
+
+    public class DeleteADigitTests
+    {
+        private readonly DeleteADigit digitDeleter;
+
+        private const int MIN = 10;
+        private const int MAX = 1000000;
+
+        public DeleteADigitTests()
+        {
+            this.digitDeleter = new DeleteADigit();
+        }
+
+        [Fact]
+        public void DeleteADigitReturnsCorrectValue()
+        {
+            AssertAreEqual(digitDeleter.DeleteDigit(10), 1);
+            AssertAreEqual(digitDeleter.DeleteDigit(152), 52);
+            AssertAreEqual(digitDeleter.DeleteDigit(1001), 101);
+        }
+
+        [Fact]
+        public void PassingAnIntegerOutOfRangeShouldThrow()
+        {
+            AssertThatThrows(MIN - 1);
+            AssertThatThrows(MAX + 1);
+        }
+
+        private void AssertThatThrows(int number)
+            => Assert.Throws<ArgumentOutOfRangeException>(delegate { digitDeleter.DeleteDigit(number); });
+
+        private void AssertAreEqual(int actual, int expected) => Assert.Equal(expected, actual);
+    }
+}
