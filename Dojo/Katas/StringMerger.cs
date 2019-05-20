45cfd82caf98afd5b6eced110db68ac7d9f186b5
Petar Kanev <petar.kanev@derivco.co.uk>
2019-05-20 12:00:37 +0100

    Added StringMerger kata

---
+++ Dojo/Katas/StringMerger.cs
@@ -0,0 +1,24 @@
+﻿namespace Katas
+{
+    public class StringMerger
+    {
+        public static bool isMerge(string word, string partA, string partB)
+        {
+            if (string.IsNullOrEmpty(word))
+                return string.IsNullOrEmpty(partA) && string.IsNullOrEmpty(partB);
+
+            char letter = word[0];
+            bool foundMerge = false;
+
+            int indexA = partA.IndexOf(letter);
+            if (indexA != -1)
+                foundMerge = isMerge(word.Substring(1), partA.Substring(indexA + 1), partB);
+
+            int indexB = partB.IndexOf(letter);
+            if (indexB != -1)
+                foundMerge = foundMerge || isMerge(word.Substring(1), partA, partB.Substring(indexB + 1));
+
+            return foundMerge;
+        }
+    }
+}
---
+++ Dojo/Tests/StringMergerTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+{
+    using Katas;
+    using Xunit;
+
+    public class StringMergerTests
+    {
+        [Theory]
+        [InlineData("", "", "", true)]
+        [InlineData("", "", "a", false)]
+        [InlineData("a", "", "a", true)]
+        [InlineData("", "a", "a", true)]
+        [InlineData("a", "a", "a", false)]
+        [InlineData("ab", "a", "ab", true)]
+        [InlineData("", "ab", "ab", true)]
+        [InlineData("", "ab", "a", false)]
+        [InlineData("", "a", "ab", false)]
+        [InlineData("code", "wars", "codewars", true)]
+        [InlineData("cdwr", "oeas", "codewars", true)]
+        [InlineData("cod", "wars", "codewars", false)]
+        [InlineData("wars", "code", "codewars", true)]
+        [InlineData("going ", "Bananas!", "going Bananas!", true)]
+        [InlineData("going A", "Bananas!", "going Bananas!", false)]
+        public void StringMergerReturnsCorrectValue(string partA, string partB, string merger, bool expected)
+        {
+            bool result = StringMerger.isMerge(merger, partA, partB);
+            Assert.Equal(expected, result);
+        }
+    }
+}
