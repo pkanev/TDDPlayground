15dcfd5d892c6a8d69bb5d9139b551f8757f0f18
Petar Kanev <pvkanev@gmail.com>
2019-05-27 13:46:26 +0100

    Added PrimeDecomposition

---
+++ Dojo/Katas/PrimeDecomposition.cs
@@ -0,0 +1,67 @@
+﻿namespace Katas
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class PrimeDecomposition
+    {
+        public static string Factors(int number)
+        {
+            List<int> primeNumbers = new List<int>() { 2 };
+            int factor = 2;
+            Dictionary<int, int> occurences = new Dictionary<int, int>();
+
+            while (number != 1)
+            {
+                if (number % factor == 0)
+                {
+                    number /= factor;
+                    if (!occurences.ContainsKey(factor))
+                        occurences[factor] = 0;
+
+                    occurences[factor]++;
+                }
+                else
+                {
+                    factor = GetNextPrimeNumber(factor, primeNumbers);
+                    primeNumbers.Add(factor);
+                }
+            }
+
+            return GenerateResult(occurences);
+        }
+
+        private static int GetNextPrimeNumber(int factor, List<int> primeNumbers)
+        {
+            int number = factor + 1;
+            bool isPrime;
+            do
+            {
+                isPrime = true;
+                foreach (int primeNumber in primeNumbers)
+                {
+                    if (number % primeNumber == 0 && primeNumber <= Math.Sqrt(number))
+                    {
+                        isPrime = false;
+                        number++;
+                        break;
+                    }
+                }
+            } while (!isPrime);
+
+            return number;
+        }
+
+        private static string GenerateResult(Dictionary<int, int> occurences)
+        {
+            string result = string.Empty;
+            foreach (var kvp in occurences)
+                result += BuildFactorAsString(result, kvp.Key, kvp.Value);
+
+            return result;
+        }
+
+        private static string BuildFactorAsString(string result, int prime, int power)
+            => power == 1 ? $"({prime})" : $"({prime}**{power})";
+    }
+}
---
+++ Dojo/Tests/PrimeDecompositionTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests
+{
+    using Katas;
+    using Xunit;
+
+    public class PrimeDecompositionTests
+    {
+        [Theory]
+        [InlineData(2, "(2)")]
+        [InlineData(4, "(2**2)")]
+        [InlineData(3, "(3)")]
+        [InlineData(8, "(2**3)")]
+        [InlineData(9, "(3**2)")]
+        [InlineData(30, "(2)(3)(5)")]
+        [InlineData(86240, "(2**5)(5)(7**2)(11)")]
+        [InlineData(7775460, "(2**2)(3**3)(5)(7)(11**2)(17)")]
+        public void FactorReturnsCorrectValues(int number, string expected)
+        {
+            string result = PrimeDecomposition.Factors(number);
+            Assert.Equal(expected, result);
+        }
+    }
+}
